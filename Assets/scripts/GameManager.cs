74a48c3dd20d84a924a65211516565fcd40ad2d3
Nobo Bhowmik <nobonita.bhowmik@outlook.com>
2018-03-09 11:27:59 -0500

    fixing assets + gameplay
    still breaking. not too sure why.

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject pupPrefab;
+    public GameObject brickPrefab;
+    public GameObject cactusPrefab;
+
+    public float tileWidth = 2f;
+    public float tileHeight = 2f;
+
+    public string levelFile = "lvl.txt";
+
+    // Use this for initialization
+    void Start()
+    {
+
+        // Reading the file into string.
+        string levelString = File.ReadAllText(Application.dataPath + Path.DirectorySeparatorChar + levelFile);
+
+        // Splitting the string into lines.
+        string[] levelLines = levelString.Split('\n');
+        int width = 0;
+        // Iterating over the lines.
+        for (int row = 0; row < levelLines.Length; row++)
+        {
+            string currentLine = levelLines[row];
+            width = currentLine.Length;
+            // Iterating over all the chars in a line.
+            for (int col = 0; col < currentLine.Length; col++)
+            {
+                char currentChar = currentLine[col];
+                if (currentChar == 'x')
+                {
+                    // Make a wall!
+                    GameObject wallObj = Instantiate(brickPrefab);
+                    wallObj.transform.parent = transform;
+                    wallObj.transform.position = new Vector3(col * tileWidth, -row * tileHeight, 0);
+                }
+                else if (currentChar == 'p')
+                {
+                    // Make the player!
+                    GameObject playerObj = Instantiate(pupPrefab);
+                    playerObj.transform.parent = transform;
+                    playerObj.transform.position = new Vector3(col * tileWidth, -row * tileHeight, 0);
+                }
+                else if (currentChar == 'e')
+                {
+                    // We flip a coin
+                    if (Random.value <= 0.5f)
+                    {
+                        GameObject enemyObj = Instantiate(cactusPrefab);
+                        enemyObj.transform.parent = transform;
+                        enemyObj.transform.position = new Vector3(col * tileWidth, -row * tileHeight, 0);
+                    }
+                }
+            }
+        }
+
+        float myX = -(width * tileWidth) / 2f + tileWidth / 2f;
+        float myY = (levelLines.Length * tileHeight) / 2f - tileHeight / 2f;
+        transform.position = new Vector3(myX, myY, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/scripts/player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    public KeyCode upKey = KeyCode.UpArrow;
+    public KeyCode rightKey = KeyCode.RightArrow;
+    public KeyCode downKey = KeyCode.DownArrow;
+    public KeyCode leftKey = KeyCode.LeftArrow;
+
+    public float moveSpeed = 10f;
+
+    Rigidbody2D _pup;
+
+    // Use this for initialization
+    void Start()
+    {
+        _pup = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 newVelocity = new Vector2(0, 0);
+        if (Input.GetKey(upKey))
+        {
+            newVelocity.y += moveSpeed;
+        }
+        if (Input.GetKey(rightKey))
+        {
+            newVelocity.x += moveSpeed;
+        }
+        if (Input.GetKey(downKey))
+        {
+            newVelocity.y -= moveSpeed;
+        }
+        if (Input.GetKey(leftKey))
+        {
+            newVelocity.x -= moveSpeed;
+        }
+
+        // Make sure the velocity always has the same magnitude
+        newVelocity = newVelocity.normalized * moveSpeed;
+
+        _pup.velocity = newVelocity;
+    }
+}
